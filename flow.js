e4da5b7d8fbc20064fcb7a249c03578faf273330
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-08 20:32:06 +0300

    created flow logic

---
+++ flow.js
@@ -0,0 +1,37 @@
+// Home page
+
+const welcome = () =>{
+    // render welcome mesg and show login 
+}
+
+const login = ()=>{
+    //1.  display login modal
+    //2. Handle login
+}
+
+const select = () => {
+    //1. select year
+    //2. select department 
+    //3. select course
+    //4. Download
+}
+
+const years = ['First','second','Third','fourth']
+const departments = ['medsurg','comm health','UCU']
+
+const courses = {
+    RCH300:[RCH301,rch302],
+    UCU110,
+    UCU111
+}
+
+const showResources = () => {
+    //1. display the notes
+    // LOGIC : when clicked, the user should be able to download the notes ( display the download button )
+    //LOGIC : when clicked, the admin should display : download, delete, edit buttons    
+}
+
+const filter = () => {
+    //1. filter by notes/past papers/textbooks/course outlines    
+}
+
